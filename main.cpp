be3543579d6d3e942a1a74988eae3d5d3c6e0a67
oprisan <aoprisan@adobe.com>
2018-06-08 03:50:38 +0300

    Still in work

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <map>
+#include <utility>
+
 #include "mpi.h"
 #include "utils.h"
 
+#define MASTER 0
+
 using namespace std;
 
-static void masterProcess(int matrixDimension);
-static void slaveProcess(int matrixDimension);
+static void masterProcess(int matrixDimension, int worldSize);
+static void slaveProcess(int matrixDimension, int worldRank);
 
 int main(int argc, char* argv[]) {
 
@@ -18,27 +23,17 @@ int main(int argc, char* argv[]) {
 
     MPI_Init(&argc, &argv);
 
+    int matrixDimension = atoi(argv[1]);
+
     int worldRank, worldSize;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
     MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
 
-    if (worldRank == 0) {
-        cout << "I am the master!" << endl << flush;
-
-        for (int i = 1; i < worldSize; i++) {
-            int integerToSend = i+2;
-            MPI_Send(&integerToSend, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
-
-        cout << "I, the master, successfully send the numbers" << endl << flush;
+    if (worldRank == MASTER) {
+        masterProcess(matrixDimension, worldSize);
     } else {
-        cout << "I am not the master I am " << worldRank << endl << flush;
-        int integerToReceive;
-        MPI_Status status;
-        MPI_Recv(&integerToReceive, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-
-        cout << "I, " << worldRank << " received the number " << integerToReceive << " from: " << status.MPI_SOURCE << endl << flush;
+        slaveProcess(matrixDimension, worldRank);
     }
 
     MPI_Finalize();
@@ -46,10 +41,48 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-static void masterProcess(int matrixDimension) {
+static void masterProcess(int matrixDimension, int worldSize) {
+    cout << "I am the master!" << endl << flush;
+
+    int **firstMatrix = allocateMatrix(matrixDimension);
+    int **secondMatrix = allocateMatrix(matrixDimension);
+    int **matrixMultiplicationResult = allocateMatrix(matrixDimension);
+
+    populateMatrixWithOnes(firstMatrix, matrixDimension);
+    populateMatrixWithOnes(secondMatrix, matrixDimension);
+
+    int *rows = new int[matrixDimension];
+    int *columns = new int[matrixDimension];
+    int counter = 0;
+
+    while (counter < matrixDimension) {
+        for (int i = 1; i < worldSize && counter < matrixDimension; i++) {
+            for (int j = 0; j < matrixDimension; j++) {
+                rows[j] = firstMatrix[counter][j];
+                columns[j] = secondMatrix[j][counter];
+            }
+
+            MPI_Send(rows, matrixDimension, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(columns, matrixDimension, MPI_INT, i, 0, MPI_COMM_WORLD);
+            
+
+        }
+    }
+
+    cout << "I, the master, successfully send the numbers" << endl << flush;
+
+
+    freeMatrix(firstMatrix, matrixDimension);
+    freeMatrix(secondMatrix, matrixDimension);
+    freeMatrix(matrixMultiplicationResult, matrixDimension);
 
 }
 
-static void slaveProcess(int matrixDimension) {
+static void slaveProcess(int matrixDimension, int worldRank) {
+    cout << "I am slave number: " << worldRank << endl << flush;
+    int integerToReceive;
+    MPI_Status status;
+    MPI_Recv(&integerToReceive, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
 
+    cout << "I, " << worldRank << " received the number " << integerToReceive << " from: " << status.MPI_SOURCE << endl << flush;
 }
